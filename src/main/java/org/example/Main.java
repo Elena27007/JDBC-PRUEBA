112e1789a29e658a1cd997b9775c6eeb278b3a59
Elena27007 <elena.s370935@cesurformacion.com>
2024-10-17 11:10:38 +0200

    ea

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,9 @@ import org.example.model.Pelicula;
 
 public class Main {
     public static void main(String[] args) {
-        new PeliculaDAO(JdbcUtils.getConnection()).save(new Pelicula(7,"From", 2017, "terror"));
+        //new PeliculaDAO(JdbcUtils.getConnection()).save(new Pelicula(7,"From", 2017, "terror"));
         Integer c = new PeliculaDAO(JdbcUtils.getConnection()).countFilms();
-        System.out.println(c);
-        new PeliculaDAO(JdbcUtils.getConnection()).filterByGenre("accion").forEach(System.out::println);
+        System.out.println("Número de películas: "+ c);
+        //new PeliculaDAO(JdbcUtils.getConnection()).filterByGenre("accion").forEach(System.out::println);
     }
 }
